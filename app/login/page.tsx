ffa60a5d6939c8f0ff77cbb72fba83478eaa1612
RafaelLechensque <rafaelaquino@alunos.utfpr.edu.br>
2025-01-20 19:24:56 -0300

    add forms

---
+++ app/login/page.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+  email: z.string().email({ message: 'email obrigatorio' }),
+  password: z
+    .string()
+    .min(6, { message: 'minino 6 caracteres' })
+    .max(20, { message: 'maximo 20 caracteres' }),
+});
+
+export default function Login() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(value: z.infer<typeof formSchema>) {
+    console.log(value);
+  }
+  return (
+    <div className="w-1/4 m-auto mt-52 border-2 border-slate-700 p-11 rounded-lg">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input placeholder="Insira seu email" {...field} />
+                </FormControl>
+                {/* <FormDescription>
+                  This is your public display name.
+                </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Senha</FormLabel>
+                <FormControl>
+                  <Input placeholder="Insira sua senha" {...field} />
+                </FormControl>
+                {/* <FormDescription>
+                  This is your public display name.
+                </FormDescription> */}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Logar</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
